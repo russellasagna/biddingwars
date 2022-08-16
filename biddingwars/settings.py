997386dc0e46374247aee54f2abf1adc4d7eda62
Andrewbui389 <andrewbui389@gmail.com>
2022-08-16 11:52:25 -0700

    New Sync

--- biddingwars/settings.py
+++ biddingwars/settings.py
@@ -124,6 +124,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = ''
+LOGIN_REDIRECT_URL = '/'
 
-LOGOUT_REDIRECT_URL = ''
+LOGOUT_REDIRECT_URL = '/'
