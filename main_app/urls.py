eb7fec02158c66e70e74e7345c3916c560b03bc0
russellasagna <laganzon.r2@yahoo.com>
2022-08-17 12:54:39 -0400

    Finishing CRUD

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,11 +4,12 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('bids/userbids', views.user_bids, name='user_bids'),
-    path('bids/', views.bid_list, name='bid_index'),
-    path('bids/create/', views.BidCreate.as_view(), name='bid_create'),
-    path('bids/<int:pk>/update/', views.BidUpdate.as_view(), name='bid_update'),
-    path('bids/<int:pk>/delete/', views.BidDelete.as_view(), name='bid_delete'),
-    path('bids/<int:sell_id>/', views.bid_detail, name='bid_detail'),
+    path('bids/', views.post_list, name='post_list'),
+    path('bids/create/', views.PostCreate.as_view(), name='post_create'),
+    # path('bids/create/buyer/', views.add_buyer, name='new_buyer'),
+    path('bids/<int:pk>/update/', views.PostUpdate.as_view(), name='post_update'),
+    path('bids/<int:pk>/delete/', views.PostDelete.as_view(), name='post_delete'),
+    path('bids/<int:sell_id>/', views.post_detail, name='post_detail'),
     path('accounts/signup/', views.signup, name='signup'),
 
 ]
